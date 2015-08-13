cf7c8cafa79acccbd43565d0e34bf5af0077f4b6
ykim733 <ykim733@gmail.com>
2015-08-12 20:56:10 -0500

    datastore is sort of working

--- octoadventure/main.py
+++ octoadventure/main.py
@@ -14,12 +14,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.ext import db
 import webapp2
+import jinja2
+import os
+from google.appengine.api import users
+import sys
 
-class MainHandler(webapp2.RequestHandler):
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class UserModel(db.Model):
+    currentUser = db.StringProperty(required = True)
+
+class UserInfoModel(db.Model):
+    first_name = db.StringProperty(required = True)
+    last_name = db.StringProperty(required = True)
+    phone_number = db.PhoneNumberProperty(required = True)
+    email = db.EmailProperty(required = True)
+    dorm_building = db.StringProperty(required = True)
+    # dorm_room_number = db.IntegerProperty()
+
+class UserInfoHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user_info_template = jinja_environment.get_template('templates/user.html')
+        self.response.out.write(user_info_template.render())
+
+        user = users.get_current_user()
+
+        if user:
+            self.response.write('<br><br>' + "Welcome, ")
+            self.response.write(str(user) + '<br><br><br>')
+            user = UserModel(currentUser = user.user_id())
+            user.put()
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+    def post(self):
+        user_firstname = self.request.get("firstname")
+        user_lastname = self.request.get("lastname")
+        user_phone = self.request.get("number")
+        user_email = self.request.get("email")
+        user_building = self.request.get("building")
+        user_room = self.request.get("room")
+        form = UserInfoModel(first_name = user_firstname, last_name = user_lastname, phone_number = user_phone, email = user_email, dorm_building = user_building, dorm_room_number = user_room)
+        form.put()
+        redirect_template = jinja_environment.get_template('templates/user.html')
+        self.response.out.write(redirect_template.render())
+
+# class HomepageHandler(webapp2.RequestHandler):
+#     def get(self):
+#         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', UserInfoHandler)
 ], debug=True)

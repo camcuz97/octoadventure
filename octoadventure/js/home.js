4f1f51ffb4f168ec8d0530a8d9bcc5ccee4f7f0f
ykim733 <ykim733@gmail.com>
2015-08-19 17:08:31 -0500

    Revert "more animation"
    This reverts commit e282e54e5755a0d289424d3b2adaabd1d7d7ccc3.

--- octoadventure/js/home.js
+++ octoadventure/js/home.js
@@ -1,34 +1,7 @@
 $(document).ready(function(){
   $("h1").click(function(){
-    $(this).fadeOut(1000).delay(3000).fadeIn(1000);
+    $(this).fadeOut();
   });
 });
 
 // testing jQuery
-
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;
-
-  if ($(window).scrollTop() >= (orgElementTop)) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.
-    orgElement = $('.original');
-    coordsOrgElement = orgElement.offset();
-    leftOrgElement = coordsOrgElement.left;
-    widthOrgElement = orgElement.css('width');
-    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-    $('.original').css('visibility','hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').hide();
-    $('.original').css('visibility','visible');
-  }
-}
-
-$(document).ready(function() {
-  $('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-  scrollIntervalID = setInterval(stickIt, 10);
-})
